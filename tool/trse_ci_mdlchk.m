00d10c125a798cdd7c000faa9db8602cea51f140
YosukeMizunoTR <yosuke.mizuno@exc.tokai-rika.co.jp>
2026-07-30 15:44:17 +0900

    report

--- tool/trse_ci_mdlchk.m
+++ tool/trse_ci_mdlchk.m
@@ -20,7 +20,7 @@ function trse_ci_mdlchk(FileName,ModelName)
 	    	fprintf('Result NG...\n');
 	        fprintf('%s\n',Rlt.Comment);
 	        fprintf('GuideLineCheck end\n');
-	        exit(2);
+	        %exit(2);
 	        
 	    else
 	    
