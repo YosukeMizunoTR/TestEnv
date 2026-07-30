cb01a93b3ebf1d9c86520c05b211fbbc560b513e
YosukeMizunoTR <yosuke.mizuno@exc.tokai-rika.co.jp>
2026-07-30 12:52:03 +0900

    trrc用ci作成

--- tool/trse_ci_mdlenv.m
+++ tool/trse_ci_mdlenv.m
@@ -1,21 +1,15 @@
 function trse_ci_mdlenv(FileName,ModelName)
+	
+	disp('model setup start');
+	try
+		
+		Rlt = trse_mdlenv(FileName,ModelName);
 
-	try
-	  evalc('run(''trrc'')');
-	  disp('MATLAB(mbd_common) setup OK');
-	catch ME
-		disp('%s\n', ME.message);
-		exit(3);
-	end
-	
-	Rlt = trse_mdlenv(FileName,ModelName);
-	
-	try
 	    if ~Rlt.Xresult %結果NG
 	        disp(Rlt.Comment);
 	        exit(100);
 	    else
-	    	disp('model setup OK');
+	    	disp('model setup end');
 	    end
 	    
 	catch ME
