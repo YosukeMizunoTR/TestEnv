9873b18971c81a79c2b833486aabc50c6f4d3fe5
YosukeMizunoTR <yosuke.mizuno@exc.tokai-rika.co.jp>
2026-07-30 13:31:17 +0900

    test

--- tool/trse_ci_mdlchk.m
+++ tool/trse_ci_mdlchk.m
@@ -3,7 +3,7 @@ function trse_ci_mdlchk(FileName,ModelName)
 	disp('GuideLineCheck start');
 	trse_ci_mdlenv(FileName,ModelName);
 
-    Rlt = trse_mdlenv(FileName,ModelName,'Full');
+    Rlt = trse_mdlchk(FileName,ModelName,'Full');
 
     if ~Rlt.Xresult %結果NG
         disp(Rlt.Comment);
