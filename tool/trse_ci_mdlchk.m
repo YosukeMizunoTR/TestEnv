da8a75311c7d1da6b451f6bcd60648649f1bf9ea
YosukeMizunoTR <yosuke.mizuno@exc.tokai-rika.co.jp>
2026-07-30 13:09:11 +0900

    スクリプト修正

--- tool/trse_ci_mdlchk.m
+++ tool/trse_ci_mdlchk.m
@@ -1,11 +1,13 @@
 function trse_ci_mdlchk(FileName,ModelName)
 
-	trse_ci_mdlenv(FileName,ModelName);
+	disp('GuideLineCheck start');
+	trse_ci_mdlenv(FileName,ModelName,true);
 
     Rlt = trse_mdlenv(FileName,ModelName,'Full',true);
 
     if ~Rlt.Xresult %結果NG
         disp(Rlt.Comment);
+        disp('GuideLineCheck end');
         exit(2);
     end
 
--- tool/trse_ci_mdlenv.m
+++ tool/trse_ci_mdlenv.m
@@ -1,15 +1,15 @@
 function trse_ci_mdlenv(FileName,ModelName)
 	
-	disp('model setup start');
+	disp('MODEL setup start');
 	try
 		
-		Rlt = trse_mdlenv(FileName,ModelName);
+		Rlt = trse_mdlenv(FileName,ModelName,true);
 
 	    if ~Rlt.Xresult %結果NG
 	        disp(Rlt.Comment);
 	        exit(100);
 	    else
-	    	disp('model setup end');
+	    	disp('MODEL setup end');
 	    end
 	    
 	catch ME
